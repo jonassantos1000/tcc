1d68170c5a4730fc99e6d5e71b30cb25ed7e5b05
jonassantos1000 <jonasvale3@hotmail.com>
2022-09-09 21:06:26 -0300

    [FEAT]: REFATORADO CODIGO JS

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,72 +1,80 @@
 const loader = document.querySelector("#loading");
 
-function exibirCVE(dataset){
-    document.querySelector('#div_erro').classList.add("d-none");
+function exibirCVE(dataset) {
+  document.querySelector("#div_erro").classList.add("d-none");
 
-    let cve = document.querySelector('#cve');
-    let titulo_cve = document.querySelector("#titulo_cve")
-    let resumo_cve = document.querySelector("#resumo_cve")
+  let cve = document.querySelector("#cve");
+  let titulo_cve = document.querySelector("#titulo_cve");
+  let resumo_cve = document.querySelector("#resumo_cve");
 
-    cve.classList.remove("d-none");
-    cve.classList.add("d-flex");
+  cve.classList.remove("d-none");
+  cve.classList.add("d-flex");
 
-    titulo_cve.innerHTML = dataset.id
-    resumo_cve.innerHTML = dataset.resume
+  titulo_cve.innerHTML = dataset.id;
+  resumo_cve.innerHTML = dataset.resume;
 }
-function exibirErro(dataset){
-     document.querySelector('#cve').classList.add("d-none");
+function exibirErro(titulo, descricao) {
+  document.querySelector("#cve").classList.add("d-none");
 
-    let div_erro = document.querySelector('#div_erro');
-    let titulo_cve = document.querySelector("#titulo_erro")
-    let resumo_cve = document.querySelector("#resumo_erro")
+  let div_erro = document.querySelector("#div_erro");
+  let titulo_cve = document.querySelector("#titulo_erro");
+  let resumo_cve = document.querySelector("#resumo_erro");
 
-    div_erro.classList.remove("d-none");
-    div_erro.classList.add("d-flex");
+  div_erro.classList.remove("d-none");
+  div_erro.classList.add("d-flex");
 
-    resumo_cve.innerHTML = "Insira um CVE válido!"
-    titulo_cve.innerHTML = dataset.response
-    if (dataset.description){
-        resumo_cve.innerHTML = dataset.description
-    }
+  resumo_cve.innerHTML = descricao;
+  titulo_cve.innerHTML = titulo;
 }
-function getValueSearchBar(){
-    return document.querySelector('#barra_pesquisa_cve').value;
+function getValueSearchBar() {
+  return document.querySelector("#barra_pesquisa_cve").value;
 }
 
 function displayLoading() {
-    loader.classList.add("display");
+  loader.classList.add("display");
 }
 
 function hideLoading() {
-    loader.classList.remove("d-none");
-    loader.classList.remove("display");
+  loader.classList.remove("d-none");
+  loader.classList.remove("display");
 }
 
-function getCVE(){
-    displayLoading()
+function cveEhValido() {
+  if (!getValueSearchBar()) {
+    hideLoading();
+    exibirErro(
+      "Informação pendente",
+      "É necessario informar um CVE para prosseguir com a consulta."
+    );
+    return false;
+  }
+  return true;
+}
 
-    if (!getValueSearchBar()){
-        hideLoading()
-        dataset = {response: 'Informação pendente', description: "É necessario informar um CVE para prosseguir com a consulta."}
-        exibirErro(dataset)
-        return
-    }
-
+function getCVE() {
+  if (cveEhValido()) {
+    displayLoading();
     fetch(`http://192.168.1.220:5000/vistoria/${getValueSearchBar()}`)
-    .then(response => {
-        hideLoading()
-        if(response.ok){
-            response.json().then((response) => {
-                exibirCVE(response)
-            })
+      .then((dataset) => {
+        hideLoading();
+        if (dataset.ok) {
+          dataset.json().then((dataset) => {
+            exibirCVE(dataset);
+          });
         } else {
-           response.json().then((response) => exibirErro(response));
+          response
+            .json()
+            .then((dataset) =>
+              exibirErro("Insira um CVE válido!", dataset.response)
+            );
         }
-
-    })
-    .catch(function(error) {
-        hideLoading()
-        dataset = {response: 'Ocorreu um erro', description: error.message}
-        exibirErro(dataset)
-    });
+      })
+      .catch(function (error) {
+        hideLoading();
+        return exibirErro(
+          "Erro de conexão",
+          "Não foi possivel se conectar ao servidor"
+        );
+      });
+  }
 }
