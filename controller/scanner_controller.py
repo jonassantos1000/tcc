61872d4f53f2a2a756d441fb1b7780eb0b1b53ba
jonassantos1000 <jonasvale3@hotmail.com>
2022-09-10 13:48:59 -0300

    [FEAT]: TESTANDO ESTILIZAÇÃO NA BARRA DE ROLAGEM

--- controller/scanner_controller.py
+++ controller/scanner_controller.py
@@ -18,3 +18,7 @@ def findById(cve):
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/vistoria', methods=['GET'])
+def vistoria():
+    return render_template('vistoria.html')
